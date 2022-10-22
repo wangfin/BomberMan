2a8da800d00e7f42d6f4e3ecd6993e27ae30c5c7
wangfin <769790863@qq.com>
2022-10-22 14:58:21 +0800

    创建项目

---
+++ bomberMan/src/test/java/com/eastmoney/bomberman/BomberManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eastmoney.bomberman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomberManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
